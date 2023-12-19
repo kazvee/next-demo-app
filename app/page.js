2e3cf9e5639d1adfb0b951fa0ec9452814f39dee
kazvee <kazveedev@gmail.com>
2023-12-18 21:12:15 -0700

    Refactor site header to a separate component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to the NextJS Recipe Share App!</h1>
+      <Header />
       <p>🔥 Let&apos;s get cooking! 🔥</p>
       <p>
         <Link href="/about">About Us</Link>
